af4b3795ce26d1a3017a3ff3d0e390e50983b68f
Frankie Ha <126730331+fankiha@users.noreply.github.com>
2023-10-23 12:01:14 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -224,17 +224,6 @@ if ($conn->connect_error) {
 								</article>
 							</section>
 
-						<!-- Footer 
-							<footer>
-								<div class="pagination">
-									<a href="#" class="previous">Prev</a>
-									<a href="#" class="page active">1</a>
-									<a href="#" class="page">2</a>
-									<a href="#" class="page">3</a>
-									<a href="#" class="next">Next</a>
-								</div>
-							</footer>
-							//-->
 					</div>
 
 				<!-- Footer -->
