0dd214c4c1e3b3883c862d44f6d492b2c6c9e6e8
Frankie Ha <126730331+fankiha@users.noreply.github.com>
2023-10-24 14:25:05 -0600

    Add files via upload

--- password.php
+++ password.php
@@ -1,18 +1,3 @@
-<?php
-$host = "localhost";
-$username = "db_francci";
-$password = "6S#BN%5sfg";
-$dbname = "db_francci";
-
-// Create a database connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
 <!DOCTYPE HTML>
 <!--
 	Massively by HTML5 UP
@@ -114,172 +99,181 @@ if ($conn->connect_error) {
 						
 					</nav>
 
-                    				<!-- Main -->
+				<!-- Main -->
 					<div id="main">
 
-                    <!-- Post -->
-                        <section class="post">
-                            <header class="major">
-                                <h1>Change Password</h1>
-                            </header>
+						<!-- Post -->
+							<section class="post">
+								<header class="major">
+								</header>
 
-                                
+<?php
 
-							<?php
-
-                            			// Check if the "my_cookie" cookie is set
-										if (isset($_COOKIE['my_cookie'])) {
-											// Get the cookie value and split it into individual values
-											$cookieValue = $_COOKIE['my_cookie'];
-											$cookieValues = explode('|', $cookieValue);
-
-
-                                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                                                // Check if the form has been submitted via POST
+if (isset($_COOKIE['my_cookie'])) {
+    // Decode the cookie value (if it's JSON, for example)
+    $cookieData = json_decode($_COOKIE['my_cookie'], true);
     
-                                                // Retrieve the values from the form fields
-                                                $oldPassword = $_POST["old_password"];
-                                                $newPassword = $_POST["new_password"];
-                                                $confirmPassword = $_POST["confirm_password"];
-    
-                                                // Check if the "New Password" and "Confirm New Password" fields match
-                                                if ($newPassword === $confirmPassword) {
-    
-                                                    $sql = "SELECT * FROM user_info where user_id = $uid AND password = '$oldPassword';";
-    
-                                                    $result = $conn->query($sql);
-    
-                                                    if ($result->num_rows > 0) {
-                                                        
-                                                        while ($row = $result->fetch_assoc()) {
-    
-                                                            $sql = "UPDATE `user_info` SET `password` = '$newPassword' WHERE user_id = '$uid'";
-    
-                                                            // Execute the query
-                                                            if ($conn->query($sql) === TRUE) {
-                                                                // Clear the cookie by setting its expiration date to the past
-                                                                setcookie('my_cookie', '', time() - 3600, '/');
-                                                                echo "Password updated successfully. Redirecting to login page in 5 seconds...";
-                                                                echo '<script type="text/javascript">
-                                                                    setTimeout(function () {
-                                                                        window.location.href = "login.html";
-                                                                    }, 5000); // 5000 milliseconds = 5 seconds
-                                                                </script>';
-                                                            } else {
-                                                                echo "Error updating password: " . $conn->error;
-                                                            }
-    
-                                                            // Close the database connection
-                                                            $conn->close();
-                                                        }
-                                                    } else {
-    
-                                                        echo "You old password is incorrect.";
-                                                    }
-    
-                                                    // Passwords match, you can proceed with updating the user's password in the database
-    
-                                                    // Make sure to securely hash the new password before saving it to the database
-                                                    // For example, using password_hash() function
-    
-                                                    // Then, you can update the password in your database
-                                                    // Example: $newHashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-                                                    // Update user's password with $newHashedPassword in the database
-    
-                                                   // echo "Password has been updated successfully.";
-                                                } else {
-                                                    // Passwords do not match, handle the error or provide feedback to the user
-                                                    echo "New Password and Confirm Password do not match.";
-                                                }
-                                            }
+    // Check if the decoding was successful
+    if ($cookieData !== null) {
+        // Iterate through and echo the data
+        foreach ($cookieData as $key => $value) {
+            echo "$key: $value<br>";
+        }
+    } else {
+       // Redirect to profileupdate.php
+	   header("Location: profileupdate.php");
+	   exit;
+    }
+} else {
+	echo '<div style="text-align:center; margin-top: 50px;"><H2>Please login.</H2></div>';
+}
 
 
+$host = "ls-5d65c83575404b171779b0657bc9f2f90f9cf69e.cjvvc5r4aih0.us-east-1.rds.amazonaws.com";
+$username = "dbmasteruser";
+$password = "{<g]+q6WsOLnzt].e4`Nb#g%[z<8Jnfa";
+$dbname = "db_francci";
 
-                                            }
-                                        else
-                                        {
-                                            echo "You are not allow to see this page";
-                                        }
-										?>
+// Create a database connection
+$conn = new mysqli($host, $username, $password, $dbname);
 
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $uid = $_POST["uid"];
+    $oldPassword = $_POST["old_password"];
+    $newPassword = $_POST["new_password"];
+    $confirmPassword = $_POST["confirm_password"];
+
+    // Verify that the new password matches the confirm password
+    if ($newPassword != $confirmPassword) {
+		echo '<div style="text-align:center; margin-top: 50px;"><H2>New password and confirm password do not match.  Please try again.</H2></div>';
+		echo '<div style="text-align:center; margin-top: 50px;"><a href="#" onclick="goBack()">Back</a></div>';
+    } else {
+        // Sanitize and validate user inputs (e.g., check for length, format, etc.)
+
+        // Use prepared statements for better security
+        $sql = "SELECT * FROM user_info WHERE user_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $uid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if ($row["password"] == $oldPassword) {
+                // Update the user's password in the database
+                $updateSql = "UPDATE user_info SET password = ? WHERE user_id = ?";
+                $updateStmt = $conn->prepare($updateSql);
+                $updateStmt->bind_param("ss", $newPassword, $uid);
+
+				if ($updateStmt->execute()) {
+					// Display "Password change success" in the middle of the page
+					echo '<div style="text-align:center; margin-top: 50px;"><H2>Password change success.</H2><br> You will be redirect to the main page in 5 seconds...</div>';
+
+					// Reset the "my_cookie" using JavaScript
+					echo '<script>
+							setTimeout(function() {
+								document.cookie = "my_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+								window.location.href = "index.php";
+							}, 5000); // 5 seconds
+						</script>';
+				} else {
+					echo '<div style="text-align:center; margin-top: 50px;"><H2>Failed to update the password.</H2></div>';
+				}
+
+                $updateStmt->close();
+            } else {
+				echo '<div style="text-align:center; margin-top: 50px;"><H2>Your old password is incorrect. Please try again.</H2></div>';
+				echo '<div style="text-align:center; margin-top: 50px;"><a href="#" onclick="goBack()">Back</a></div>';
+            }
+        } else {
+			echo '<div style="text-align:center; margin-top: 50px;"><H2>No data found in the user_info table.</H2></div>';
+        }
+
+        $stmt->close(); // Close the prepared statement
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
+
+</div>
+
+<!-- Footer -->
+	<footer id="footer">
+		<section>
+			<form method="post" action="#">
+				<div class="fields">
+					<div class="field">
+						<label for="name">Name</label>
+						<input type="text" name="name" id="name" />
 					</div>
-
-				<!-- Footer -->
-					<footer id="footer">
-						<section>
-							<form method="post" action="#">
-								<div class="fields">
-									<div class="field">
-										<label for="name">Name</label>
-										<input type="text" name="name" id="name" />
-									</div>
-									<div class="field">
-										<label for="email">Email</label>
-										<input type="text" name="email" id="email" />
-									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="3"></textarea>
-									</div>
-								</div>
-								<ul class="actions">
-									<li><input type="submit" value="Send Message" /></li>
-								</ul>
-							</form>
-						</section>
-						<section class="split contact">
-							<section class="alt">
-								<h3>Address</h3>
-								<p>1234 Somewhere Road #87257<br />
-								Nashville, TN 00000-0000</p>
-							</section>
-							<section>
-								<h3>Phone</h3>
-								<p><a href="#">(000) 000-0000</a></p>
-							</section>
-							<section>
-								<h3>Email</h3>
-								<p><a href="#">info@untitled.tld</a></p>
-							</section>
-						</section>
-					</footer>
-
-				<!-- Copyright -->
-					<div id="copyright">
-						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					<div class="field">
+						<label for="email">Email</label>
+						<input type="text" name="email" id="email" />
 					</div>
+					<div class="field">
+						<label for="message">Message</label>
+						<textarea name="message" id="message" rows="3"></textarea>
+					</div>
+				</div>
+				<ul class="actions">
+					<li><input type="submit" value="Send Message" /></li>
+				</ul>
+			</form>
+		</section>
+		<section class="split contact">
+			<section class="alt">
+				<h3>Address</h3>
+				<p>1234 Somewhere Road #87257<br />
+				Nashville, TN 00000-0000</p>
+			</section>
+			<section>
+				<h3>Phone</h3>
+				<p><a href="#">(000) 000-0000</a></p>
+			</section>
+			<section>
+				<h3>Email</h3>
+				<p><a href="#">info@untitled.tld</a></p>
+			</section>
+		</section>
+	</footer>
 
-			</div>
+<!-- Copyright -->
+	<div id="copyright">
+		<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+	</div>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-			<script>
-			function logout() {
-			// Delete the cookie by setting its expiration date to the past
-			document.cookie = "my_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+</div>
 
-			// Redirect the user to index.php
-			window.location.href = "index.php";
-			}
-			</script>
-			<script>
-			function goBack() {
-			// Use the history object to go back one page in the browsing history
-			window.history.back();
-			}
-			</script>
-			<script>
-				// JavaScript function to open a new window when the button is clicked
-				document.getElementById("openNewWindowButton").addEventListener("click", function() {
-					window.open("password.php", "_blank");
-				});
-			</script>
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+<script>
+function logout() {
+// Delete the cookie by setting its expiration date to the past
+document.cookie = "my_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-	</body>
+// Redirect the user to index.php
+window.location.href = "index.php";
+}
+</script>
+<script>
+function goBack() {
+// Use the history object to go back one page in the browsing history
+window.history.back();
+}
+</script>
+
+</body>
 </html>
