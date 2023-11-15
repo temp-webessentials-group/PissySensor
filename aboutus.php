a4661cc770bd10d252fff9177384506603c53f6d
NateLAnderson <nathon.anderson@edu.sait.ca>
2023-11-15 13:56:54 -0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -110,11 +110,11 @@ if ($conn->connect_error) {
 					</header>
 
 				<!-- Nav -->
-					<nav id="nav">
+					<<nav id="nav">
 						<ul class="links">
-							<li class="active"><a href="index.php">Air Quality</a></li>
-							<li><a href="Registration.html">User Registration</a></li>
-							<li><a href="elements.html">Documentation</a></li>
+							<li><a href="index.php">Air Quality</a></li>
+							<li><a href="Registration_new.php">User Registration</a></li>
+							<li><a href="elements.php">Documentation</a></li>
 
 							<?php
 							if (isset($_COOKIE['my_cookie'])) {
@@ -124,18 +124,16 @@ if ($conn->connect_error) {
 								$did = $cookieValues[6];
 
 								if ($did == "99999") {
-									echo '<li><a href="admin.php">Portal Page</a></li>'; 
+									echo '<li class="active"><a href="admin.php">Portal Page</a></li>'; 
 								} else {
-									echo '<li><a href="user.php">Portal Page</a></li>'; 
+									echo '<li class="active"><a href="user.php">Portal Page</a></li>'; 
 								}
 								echo '<li><a href="#" onclick="logout()">Logout</a></li>';
 							}
 							else{
-								echo '<li><a href="login.html">Login</a></li>';
+								echo '<li><a href="login_new.php">Login</a></li>';
 							}
 							?>
-
-<!--							<li><a href="login.html">Login</a></li> -->
 						</ul>
 						
 					</nav>
